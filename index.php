b466b474bddd88fc8b95921be8d928a54ddb08fe
Hebert Soares <hebertsoaresof@gmail.com>
2023-11-28 10:35:17 -0300

    Configurando auth.php

--- index.php
+++ index.php
@@ -19,8 +19,9 @@
   
 </head>
 <body>
-  <h1>Testando</h1>
-  <i class="far fa-plus-square"></i>
+  <header>
+    <nav id="main-navbar"></nav>
+  </header>
   <!-- Bootstrap JS -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.2/js/bootstrap.js" integrity="sha512-ipBeSMCnlAvS4AEbycy0nTk9KkYr5lUJwFHNvf4IxtV/CDW4qx53mZKUryWtNr6GFaBl11EXyrU6iE3mo6ib2g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
