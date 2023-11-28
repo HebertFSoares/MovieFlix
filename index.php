d503baca7b9ff309c27671cc2bdf99bd51e64b09
Hebert Soares <hebertsoaresof@gmail.com>
2023-11-28 10:36:38 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,103 @@
 <?php
-  require_once("globals.php");
-  require_once("db.php");
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>MovieFlix</title>
-  <!-- Icon -->
-  <link rel="short icon" href="img/moviestar.ico" />
-  <!-- Bootstrap -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.2/css/bootstrap.css" integrity="sha512-r0fo0kMK8myZfuKWk9b6yY8azUnHCPhgNz/uWDl2rtMdWJlk7gmd9socvGZdZzICwAkMgfTkVrplDahQ07Gi0A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <!-- Css -->
-  <link rel="stylesheet" href="css/style.css">
+  require_once("templates/header.php");
+  require_once("dao/MovieDAO.php");
+
+  $movieDao = new MovieDAO($conn, $BASE_URL);
   
-</head>
-<body>
-  <header>
-    <nav id="main-navbar"></nav>
-  </header>
-  <!-- Bootstrap JS -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.2/js/bootstrap.js" integrity="sha512-ipBeSMCnlAvS4AEbycy0nTk9KkYr5lUJwFHNvf4IxtV/CDW4qx53mZKUryWtNr6GFaBl11EXyrU6iE3mo6ib2g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-</body>
-</html>
+  $userData = $userDAO->verifyToken();
+    
+  $latestMovies = $movieDao->getLatestMovies();
+
+  $actionMovies = $movieDao->getMoviesByCategory("Ação");
+
+  $comedyMovies = $movieDao->getMoviesByCategory("Comédia");
+  
+  $fantasyMovies =  $movieDao->getMoviesByCategory("Fantasia / Ficção");
+  
+  $dramaMovies =  $movieDao->getMoviesByCategory("Drama");
+  
+  $romanceMovies =  $movieDao->getMoviesByCategory("Romance");
+  
+  
+?>
+  <div id="main-container" class="container-fluid">
+    <?php if( $userData == false): ?>
+      <h2 class="section-title">Papo de Cinema</h2>
+      <p class="section-description">
+        <a href="<?= $BASE_URL ?>auth.php" class="link-text">Entrar / Registrar</a> para publicar seus filmes favoritos e acompanhar o catálago de avaliações e críticas sobre Filmes!
+      </p>
+      <div class="image-home"><img src="<?= $BASE_URL ?>img/img-home.png" alt="MovieFlix"></div>
+    <?php else: ?>
+  
+      <?php if(!$latestMovies): ?>
+        <h2 class="section-title">Olá <?= $userData->name ?> Cadastre novos filme</h2>
+        <p class="section-description">Nenhum filme foi cadastrado pelo usuário!</p>
+        <div class="image-home"><img src="<?= $BASE_URL ?>img/img-home.png" alt="MovieFlix"></div>     
+      <?php else: ?>
+        <h2 class="section-title">Filmes novos</h2>
+        <p class="section-description">Veja as críticas dos últimos filmes adicionados no MovieFlix</p>
+        <div class="movies-container">
+          <?php foreach($latestMovies as $movie): ?>
+            <?php require("templates/movie_card.php"); ?>
+          <?php endforeach; ?>
+          <?php if(count($latestMovies) === 0): ?>
+            <p class="empty-list">Ainda não há filmes cadastrados!</p>
+          <?php endif; ?>
+        </div>
+        <h2 class="section-title">Ação</h2>
+        <p class="section-description">Veja os melhores filmes de ação</p>
+        <div class="movies-container">
+          <?php foreach($actionMovies as $movie): ?>
+            <?php require("templates/movie_card.php"); ?>
+          <?php endforeach; ?>
+          <?php if(count($actionMovies) === 0): ?>
+            <p class="empty-list">Ainda não há filmes de ação cadastrados!</p>
+          <?php endif; ?>
+        </div>
+        <h2 class="section-title">Comédia</h2>
+        <p class="section-description">Veja os melhores filmes de comédia</p>
+        <div class="movies-container">
+          <?php foreach($comedyMovies as $movie): ?>
+            <?php require("templates/movie_card.php"); ?>
+          <?php endforeach; ?>
+          <?php if(count($comedyMovies) === 0): ?>
+            <p class="empty-list">Ainda não há filmes de comédia cadastrados!</p>
+          <?php endif; ?>
+        </div>
+        <h2 class="section-title">Fantasia / Ficção</h2>
+        <p class="section-description">Veja os melhores filmes de Fantasia / Ficção</p>
+        <div class="movies-container">
+          <?php foreach($fantasyMovies as $movie): ?>
+            <?php require("templates/movie_card.php"); ?>
+          <?php endforeach; ?>
+          <?php if(count($fantasyMovies) === 0): ?>
+            <p class="empty-list">Ainda não há filmes de Fantasia / Ficção cadastrados!</p>
+          <?php endif; ?>
+        </div>
+        <h2 class="section-title">Drama</h2>
+        <p class="section-description">Veja os melhores filmes de Drama</p>
+        <div class="movies-container">
+          <?php foreach($dramaMovies as $movie): ?>
+            <?php require("templates/movie_card.php"); ?>
+          <?php endforeach; ?>
+          <?php if(count($dramaMovies) === 0): ?>
+            <p class="empty-list">Ainda não há filmes de Drama cadastrados!</p>
+          <?php endif; ?>
+        </div>
+        <h2 class="section-title">Romance</h2>
+        <p class="section-description">Veja os melhores filmes de Romance</p>
+        <div class="movies-container">
+          <?php foreach($romanceMovies as $movie): ?>
+            <?php require("templates/movie_card.php"); ?>
+          <?php endforeach; ?>
+          <?php if(count($romanceMovies) === 0): ?>
+            <p class="empty-list">Ainda não há filmes de Romance cadastrados!</p>
+          <?php endif; ?>
+        </div>
+      <?php endif; ?>
+      
+    <?php endif ?>
+  </div>
+<?php
+  require_once("templates/footer.php");
+?>
